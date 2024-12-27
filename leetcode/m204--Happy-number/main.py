d50641511fa0ed72ef4cc0e6b651a14584e2e4c2
le huy <huypl259@gmail.com>
2024-12-27 10:33:51 +0700

    204--Count-Primes\

---
+++ leetcode/m204--Happy-number/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 3: return 0
+        primes = [1] * n
+        primes[0] = primes[1] = 0
+        for i in range(2, int(n**0.5) + 1):
+            if primes[i]:
+                for mul in range(i*i, n, i):
+                    primes[mul] = 0
+        return sum(primes)
+
